968f6a64b56e0efc572b46261e540ba526609081
jnpdx <john@johnnastos.com>
2022-06-10 06:35:51 -0700

    Boilerplate

--- VoiceTest/ContentView.swift
+++ VoiceTest/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var audioManager = AudioManager()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Button("Setup") {
+                audioManager.setupAudio()
+            }
+            
+            Button(audioManager.isRunning ? "Stop" : "Start") {
+                audioManager.toggleAudio()
+            }
+            .disabled(!audioManager.isSetup)
+        }
+        .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+class AudioManager: ObservableObject {
+    @Published var isSetup = false
+    @Published var isRunning = false
+    
+    func setupAudio() {
+        
+        isSetup = true
+    }
+    
+    func toggleAudio() {
+        guard isSetup else {
+            assertionFailure("Not setup")
+            return
+        }
+        if isRunning {
+            stopAudio()
+        } else {
+            startAudio()
+        }
+    }
+    
+    func startAudio() {
+        
+    }
+    
+    func stopAudio() {
+        
     }
 }
