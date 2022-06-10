50709a8667891be82d6de41cb17dd8937c06ce1b
jnpdx <john@johnnastos.com>
2022-06-10 06:29:30 -0700

    Initial Commit

---
+++ VoiceTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VoiceTest
+//
+//  Created by John Nastos on 6/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VoiceTest/VoiceTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceTestApp.swift
+//  VoiceTest
+//
+//  Created by John Nastos on 6/10/22.
+//
+
+import SwiftUI
+
+@main
+struct VoiceTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
